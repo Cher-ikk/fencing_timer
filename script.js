1a89d8bb0fc709cb319aba14a853fcac288d01c2
Cher-ikk <d.chernikova.zp@gmail.com>
2025-03-29 23:19:35 +0100

    l

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
+function set3MinTimer() {
+    var totalTime = 180000;
+    var minutes = Math.floor(totalTime / 60000);
+    var seconds = Math.floor((totalTime % 60000) / 1000);
+    document.getElementById('timer').textContent =
+        String(minutes).padStart(2, '0') + ":" +
+        String(seconds).padStart(2, '0');
+}
+
+
+
+
 function toggleTimer() {
-    let button = document.getElementById("toggleTimerBtn");
+    var StartButton = document.getElementById("toggvarimerBtn");
     
     if (!running) {
         startTimer();
-        button.innerText = "Pause";
+        StartButton.innerText = "Pause";
     } else {
         pauseTimer();
-        button.innerText = "Start";
+        StartButton.innerText = "Start";
     }
 }
+
+/*function togglePassivity() {
+    var PassivityButton = document.getElementById("togglePassivityBtn");
+    
+    if () {
+        PassivityButton.innerText = "Passivity: ON";
+    } else {
+        PassivityButton.innerText = "Passivity: OFF";
+    }
+}*/
